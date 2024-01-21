1e4620998c401b493c43b2305b43a393591a9679
unknown <abdalhadihachimi08@gamail.com>
2024-01-21 19:44:43 +0100

    tictactoe game with c#

---
+++ Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace TicTacToeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblFinalWiner = new System.Windows.Forms.Label();
+            this.lblPlay = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pct9 = new System.Windows.Forms.PictureBox();
+            this.pct8 = new System.Windows.Forms.PictureBox();
+            this.pct7 = new System.Windows.Forms.PictureBox();
+            this.pct6 = new System.Windows.Forms.PictureBox();
+            this.pct5 = new System.Windows.Forms.PictureBox();
+            this.pct4 = new System.Windows.Forms.PictureBox();
+            this.pct3 = new System.Windows.Forms.PictureBox();
+            this.pct2 = new System.Windows.Forms.PictureBox();
+            this.pct1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pct9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(478, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(456, 60);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Tic Tac Toe Game";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(63, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 46);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Turn";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFinalWiner
+            // 
+            this.lblFinalWiner.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFinalWiner.ForeColor = System.Drawing.Color.Yellow;
+            this.lblFinalWiner.Location = new System.Drawing.Point(63, 249);
+            this.lblFinalWiner.Name = "lblFinalWiner";
+            this.lblFinalWiner.Size = new System.Drawing.Size(177, 46);
+            this.lblFinalWiner.TabIndex = 17;
+            this.lblFinalWiner.Text = "Winner";
+            this.lblFinalWiner.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPlay
+            // 
+            this.lblPlay.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlay.ForeColor = System.Drawing.Color.White;
+            this.lblPlay.Location = new System.Drawing.Point(57, 188);
+            this.lblPlay.Name = "lblPlay";
+            this.lblPlay.Size = new System.Drawing.Size(183, 46);
+            this.lblPlay.TabIndex = 18;
+            this.lblPlay.Text = "Player1";
+            this.lblPlay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(63, 323);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 46);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Game";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(49, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 51);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Reset Game";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pct9
+            // 
+            this.pct9.Image = ((System.Drawing.Image)(resources.GetObject("pct9.Image")));
+            this.pct9.Location = new System.Drawing.Point(841, 448);
+            this.pct9.Name = "pct9";
+            this.pct9.Size = new System.Drawing.Size(218, 158);
+            this.pct9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct9.TabIndex = 30;
+            this.pct9.TabStop = false;
+            this.pct9.Tag = "2,2";
+            this.pct9.Click += new System.EventHandler(this.ButtonCommon);
+            // 
+            // pct8
+            // 
+            this.pct8.Image = ((System.Drawing.Image)(resources.GetObject("pct8.Image")));
+            this.pct8.Location = new System.Drawing.Point(592, 448);
+            this.pct8.Name = "pct8";
+            this.pct8.Size = new System.Drawing.Size(218, 158);
+            this.pct8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct8.TabIndex = 29;
+            this.pct8.TabStop = false;
+            this.pct8.Tag = "2,1";
+            this.pct8.Click += new System.EventHandler(this.ButtonCommon);
+            // 
+            // pct7
+            // 
+            this.pct7.Image = ((System.Drawing.Image)(resources.GetObject("pct7.Image")));
+            this.pct7.Location = new System.Drawing.Point(330, 448);
+            this.pct7.Name = "pct7";
+            this.pct7.Size = new System.Drawing.Size(218, 158);
+            this.pct7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct7.TabIndex = 28;
+            this.pct7.TabStop = false;
+            this.pct7.Tag = "2,0";
+            this.pct7.Click += new System.EventHandler(this.ButtonCommon);
+            // 
+            // pct6
+            // 
+            this.pct6.Image = ((System.Drawing.Image)(resources.GetObject("pct6.Image")));
+            this.pct6.Location = new System.Drawing.Point(841, 261);
+            this.pct6.Name = "pct6";
+            this.pct6.Size = new System.Drawing.Size(218, 158);
+            this.pct6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct6.TabIndex = 27;
+            this.pct6.TabStop = false;
+            this.pct6.Tag = "1,2";
+            this.pct6.Click += new System.EventHandler(this.ButtonCommon);
+            // 
+            // pct5
+            // 
+            this.pct5.Image = ((System.Drawing.Image)(resources.GetObject("pct5.Image")));
+            this.pct5.Location = new System.Drawing.Point(592, 261);
+            this.pct5.Name = "pct5";
+            this.pct5.Size = new System.Drawing.Size(218, 158);
+            this.pct5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct5.TabIndex = 26;
+            this.pct5.TabStop = false;
+            this.pct5.Tag = "1,1";
+            this.pct5.Click += new System.EventHandler(this.ButtonCommon);
+            // 
+            // pct4
+            // 
+            this.pct4.Image = ((System.Drawing.Image)(resources.GetObject("pct4.Image")));
+            this.pct4.Location = new System.Drawing.Point(330, 261);
+            this.pct4.Name = "pct4";
+            this.pct4.Size = new System.Drawing.Size(218, 158);
+            this.pct4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct4.TabIndex = 25;
+            this.pct4.TabStop = false;
+            this.pct4.Tag = "1,0";
+            this.pct4.Click += new System.EventHandler(this.ButtonCommon);
+            // 
+            // pct3
+            // 
+            this.pct3.Image = ((System.Drawing.Image)(resources.GetObject("pct3.Image")));
+            this.pct3.Location = new System.Drawing.Point(841, 72);
+            this.pct3.Name = "pct3";
+            this.pct3.Size = new System.Drawing.Size(218, 158);
+            this.pct3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct3.TabIndex = 24;
+            this.pct3.TabStop = false;
+            this.pct3.Tag = "0,2";
+            this.pct3.Click += new System.EventHandler(this.ButtonCommon);
+            // 
+            // pct2
+            // 
+            this.pct2.Image = ((System.Drawing.Image)(resources.GetObject("pct2.Image")));
+            this.pct2.Location = new System.Drawing.Point(592, 72);
+            this.pct2.Name = "pct2";
+            this.pct2.Size = new System.Drawing.Size(218, 158);
+            this.pct2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct2.TabIndex = 23;
+            this.pct2.TabStop = false;
+            this.pct2.Tag = "0,1";
+            this.pct2.Click += new System.EventHandler(this.ButtonCommon);
+            // 
+            // pct1
+            // 
+            this.pct1.Image = ((System.Drawing.Image)(resources.GetObject("pct1.Image")));
+            this.pct1.Location = new System.Drawing.Point(330, 72);
+            this.pct1.Name = "pct1";
+            this.pct1.Size = new System.Drawing.Size(218, 158);
+            this.pct1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct1.TabIndex = 22;
+            this.pct1.TabStop = false;
+            this.pct1.Tag = "0,0";
+            this.pct1.Click += new System.EventHandler(this.ButtonCommon);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Showcard Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(55, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 79);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Dadi";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AllowDrop = true;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(12, 551);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(290, 45);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "Hachimi Abdelhadi";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(84, 533);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 18);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "Created By :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1191, 616);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pct9);
+            this.Controls.Add(this.pct8);
+            this.Controls.Add(this.pct7);
+            this.Controls.Add(this.pct6);
+            this.Controls.Add(this.pct5);
+            this.Controls.Add(this.pct4);
+            this.Controls.Add(this.pct3);
+            this.Controls.Add(this.pct2);
+            this.Controls.Add(this.pct1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblPlay);
+            this.Controls.Add(this.lblFinalWiner);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pct9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblFinalWiner;
+        private System.Windows.Forms.Label lblPlay;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pct9;
+        private System.Windows.Forms.PictureBox pct8;
+        private System.Windows.Forms.PictureBox pct7;
+        private System.Windows.Forms.PictureBox pct6;
+        private System.Windows.Forms.PictureBox pct5;
+        private System.Windows.Forms.PictureBox pct4;
+        private System.Windows.Forms.PictureBox pct3;
+        private System.Windows.Forms.PictureBox pct2;
+        private System.Windows.Forms.PictureBox pct1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TicTacToeGame.Properties;
+
+namespace TicTacToeGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string Player1 = "Player1";
+        string Player2 = "Player2";
+        byte[,] resultPlayer1 = new byte [3, 3];
+        byte[,] resultPlayer2 = new byte [3, 3];
+        byte PlayCounter = 0;
+        bool GameOver = false;
+        enum GameChoice
+        {
+            Player1=1,
+            player2=2,
+        }
+        bool CheckIfWin(byte[,] ResultGame )
+        {
+            byte result=0 ;
+            byte scoreWiner1 = Convert.ToByte(GameChoice.Player1);
+            scoreWiner1 = (byte)(scoreWiner1 * 3);
+            byte scoreWiner2 = Convert.ToByte(GameChoice.player2);
+            scoreWiner2 = (byte)(scoreWiner2 * 3);
+
+            for (byte i=0;i<3;i++)
+            {
+                for(byte j=0;j<3;j++)
+                {
+                    result += ResultGame[i, j];
+                }
+                if(result == (scoreWiner1) || result == (scoreWiner2))
+                {
+                    return true;
+                }
+                result = 0;
+
+            }
+            for (byte i = 0; i < 3; i++)
+            {
+                for (byte j = 0; j < 3; j++)
+                {
+                    result += ResultGame[j, i];
+                }
+                if (result == (scoreWiner1) || result == (scoreWiner2))
+                {
+                    return true;
+                }
+                result = 0;
+
+            }
+            for (byte i = 0; i < 3; i++)
+            {
+                result += ResultGame[i, i];
+            }
+            if (result == (scoreWiner1) || result == (scoreWiner2))
+            {
+                return true;
+            }
+            result = 0;
+            byte r = 2;
+            for (byte i = 0; i < 3; i++)
+            {               
+                    result += ResultGame[i, r];
+                r--;
+            }
+            if (result == (scoreWiner1) || result == (scoreWiner2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        void LockedPictures(bool UnLocked)
+        {
+
+            pct1.Enabled = UnLocked;
+            pct2.Enabled = UnLocked;
+            pct3.Enabled = UnLocked;
+            pct4.Enabled = UnLocked;
+            pct5.Enabled = UnLocked;
+            pct6.Enabled = UnLocked;
+            pct7.Enabled = UnLocked;
+            pct8.Enabled = UnLocked;
+            pct9.Enabled = UnLocked;
+        }
+        void UpdateResultWiner(object sender)
+        {
+            if(PlayCounter<10)
+            {
+                string[] koko = ((PictureBox)sender).Tag.ToString().Split(',');
+                byte x = (Convert.ToByte(koko[0]));
+                byte y = (Convert.ToByte(koko[1]));
+
+                if (lblPlay.Text == Player1)
+                {
+                    resultPlayer1[x, y] = Convert.ToByte(GameChoice.Player1);
+                }
+                else
+                {
+                    resultPlayer2[x, y] = Convert.ToByte(GameChoice.player2);
+                }
+                PlayCounter++;
+            }
+         
+
+        }
+
+        void UpdatePicture(object sender)
+        {
+            if(PlayCounter <10)
+            {
+                if (lblPlay.Text == Player1)
+                {
+                    ((PictureBox)sender).Image = Resources.X;
+                }
+                else if (lblPlay.Text == Player2)
+                {
+                    ((PictureBox)sender).Image = Resources.O;
+
+                }
+                else
+                    ((PictureBox)sender).Image = Resources.question_mark_96;
+
+
+                
+            }
+        }
+        void CheckWinner()
+        {
+
+            if (lblPlay.Text == Player1)
+            {
+                if (CheckIfWin(resultPlayer1))
+                {
+                    lblFinalWiner.Text = Player1;
+                    LockedPictures(true);
+                    MessageBox.Show("Game Over", "Game Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    GameOver = true;
+
+                }
+            }
+            else
+            {
+                if (CheckIfWin(resultPlayer2))
+                {
+                    lblFinalWiner.Text = Player2;
+                    LockedPictures(true);
+                    MessageBox.Show("Game Over", "Game Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    GameOver = true;
+                }
+            }
+            
+
+            
+        }
+        void UpdateRound(object sender)
+        {
+            if(!GameOver)
+            {
+                UpdatePicture(sender);
+                UpdateResultWiner(sender);
+                if (PlayCounter > 4)
+                {
+                    CheckWinner();
+                }
+
+                if (lblPlay.Text == Player1)
+                {
+                    lblPlay.Text = Player2;
+                }
+                else
+                {
+                    lblPlay.Text = Player1;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wrong Choice", "Choice", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                LockedPictures(false);
+            }
+        }
+
+        void RestPictures()
+        {
+            pct1.Image = Resources.question_mark_96;
+            pct2.Image = Resources.question_mark_96;
+            pct3.Image = Resources.question_mark_96;
+            pct4.Image = Resources.question_mark_96;
+            pct5.Image = Resources.question_mark_96;
+            pct6.Image = Resources.question_mark_96;
+            pct7.Image = Resources.question_mark_96;
+            pct8.Image = Resources.question_mark_96;
+            pct9.Image = Resources.question_mark_96;
+
+
+        }
+
+        private void ButtonCommon(object sender, EventArgs e)
+        {
+            UpdateRound(sender);
+        }
+
+        void CleanArray(byte[,] bytes)
+        {
+            for(byte i=0;i<3;i++)
+            {
+                for (byte j = 0; j < 3; j++)
+                {
+                    bytes[i, j] = 0;
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblPlay.Text = Player1;
+            lblFinalWiner.Text = "Winner";
+            RestPictures();
+            LockedPictures(true);
+            GameOver = false;
+            CleanArray(resultPlayer1);
+            CleanArray(resultPlayer2);
+            PlayCounter = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void Form_Paint(object sender, PaintEventArgs e)
+        {
+            Color White = Color.FromArgb(255, 255, 255, 255);
+            Pen pen = new Pen(Color.White);
+            pen.Width = 13;
+
+            pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            e.Graphics.DrawLine(pen, 585, 65, 585, 650);
+            e.Graphics.DrawLine(pen, 820, 65, 820, 650);
+
+            e.Graphics.DrawLine(pen, 300, 240, 1100, 240);
+            e.Graphics.DrawLine(pen, 300, 435, 1100, 435);
+
+        }
+
+    }
+}
